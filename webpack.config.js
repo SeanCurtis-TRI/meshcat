f1f6da8f072f7e7a2ccb3b567e364f1f9bc5a18b
Sean Curtis <sean.curtis@tri.global>
2025-11-06 16:52:42 -0800

    Make main.min.js editable; tweak the materials

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = [{
         libraryTarget: 'umd'
     },
     mode: "development",
-    devtool: "eval-cheap-source-map"
+    devtool: "eval-cheap-source-map",
 }, {
     entry: './src/index.js',
     output: {
@@ -16,6 +16,14 @@ module.exports = [{
         libraryTarget: 'umd'
     },
     mode: "production",
+    optimization: {
+      // Disable minimization entirely
+      minimize: false,
+
+      // Ensure named modules and chunks are used for better readability in the bundle (optional, often default in 'development')
+      // namedModules: true,
+      // namedChunks: true,
+    },
     module: {
       rules: [
         {
